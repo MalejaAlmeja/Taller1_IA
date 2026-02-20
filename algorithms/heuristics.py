7945397892efff1607991e5bd7d01fd158d10069
nfcelis <nf.celis@uniandes.edu.co>
2026-02-20 15:21:15 -0500

    Heuristica inicial

--- algorithms/heuristics.py
+++ algorithms/heuristics.py
@@ -148,4 +148,67 @@ def MST(survivor_coordinates):
     
     
         
+"""
+Primera Version Heuristica Autonoma: 
+def survivorHeuristic(state: Tuple[Tuple, Any], problem: MultiSurvivorProblem):
+    
+    problem.heuristicInfo
+    # No quedan sobrevivientes
+    if state[1].count() == 0:
+        return 0
+    
+    min_survivor_distance = float('inf')
+    #Tomar la distancia al sobreviviente más cercano
+    grid = state[1]
+    survivor_coordinates = grid.asList()
+    for survivor in survivor_coordinates:
+        #distancia al sobreviviente más cercano con Manhattan
+        distance = abs(state[0][0] - survivor[0]) + abs(state[0][1] - survivor[1])
+        min_survivor_distance = min(min_survivor_distance, distance)
+    #la llave en heuristicInfo la calculamos como la tupla de survivor_coordinates en ese estado/ evita recalcular después 
+    key = tuple(survivor_coordinates)
+    #si no está en heuristicInfo se calcula y guarda y si ya está se saca de ahí 
+    if key not in problem.heuristicInfo:
+        problem.heuristicInfo[key] = MST(survivor_coordinates)
+    mst = problem.heuristicInfo[key]
+    
+    return min_survivor_distance + mst
         
+        
+    
+
+    #Tengo otra idea:
+    # 1. Hacer MST de los sobrevivientes restantes
+    # 2. Tomar la distancia al sobreviviente más cercano
+    # Pero no séeeeee
+    # Usar lo de problem.heuristicInfo para guardar la distancia entre sobrevivientes 
+    # y no tener que recalcularla cada vez maybe
+
+    return min_survivor_distance
+    
+def MST(survivor_coordinates):
+    #nos da el costo mínimo para visitar a los sobrevivientes restantes con distancia Manhattan
+    #si hay 0-1 el costo sería 0
+    if len(survivor_coordinates) <= 1:
+        return 0
+    visited = [survivor_coordinates[0]]
+    no_visited = survivor_coordinates[1:]
+    cost = 0 
+    
+    #visitar todos los sobrevivientes
+    while len(no_visited) > 0:
+        m_edge = float('inf')
+        next = None
+        for survivor in visited:
+            for survivor2 in no_visited:
+                #distancia entre sobreviviente y sobreviviente2 con Manhattan
+                distancia = abs(survivor[0] - survivor2[0]) + abs(survivor[1] - survivor2[1])
+                if distancia < m_edge:
+                    m_edge = distancia
+                    next = survivor2
+        cost += m_edge
+        visited.append(next)    
+        no_visited.remove(next)
+    return cost
+
+"""
